d927f4754f589831b4802878be81dcd642cdb445
Sebastien Dubois <seb@dsebastien.net>
2024-04-25 13:44:39 +0200

    Updated configs

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,37 @@
-import { readFileSync, writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from 'fs';
 
 const targetVersion = process.env.npm_package_version;
+const appFolder = './apps/plugin/src/assets';
+const targetFolder = './dist/apps/plugin';
+const manifestFile = 'manifest.json';
+const versionsFile = 'versions.json';
 
 // read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+console.log('Generating manifest');
+let manifest = JSON.parse(readFileSync(`${appFolder}/${manifestFile}`, 'utf8'));
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+writeFileSync(
+  `${appFolder}/${manifestFile}`,
+  JSON.stringify(manifest, null, '\t')
+);
+// Replace write by a copy
+writeFileSync(
+  `${targetFolder}/${manifestFile}`,
+  JSON.stringify(manifest, null, '\t')
+);
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+console.log('Updating versions file');
+let versions = JSON.parse(readFileSync(`${appFolder}/${versionsFile}`, 'utf8'));
 versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+writeFileSync(
+  `${appFolder}/${versionsFile}`,
+  JSON.stringify(versions, null, '\t')
+);
+// Replace write by a copy
+writeFileSync(
+  `${targetFolder}/${versionsFile}`,
+  JSON.stringify(versions, null, '\t')
+);
