a0adc10e307e98b4d918c20f033a9ddbfef7659e
Sebastien Dubois <seb@dsebastien.net>
2024-05-09 10:00:12 +0200

    Updated version script

--- README.md
+++ README.md
@@ -2,21 +2,10 @@
 
 Obsidian plugin that gives you the power of Dataview, but generates Markdown, making it compatible with Obsidian Publish, and making the links appear on the Graph.
 
-## TODO
+## Features
 
-- [ ] Readme
-- [ ] Remove copy:assets in package.json (maybe done by esbuild)
-
-## Installation
-
-TODO
+...
 
 ## Configuration
 
-## Contributing
-
-TODO
-
-## Development environment
-
-TODO
+...
--- version-bump.mjs
+++ version-bump.mjs
@@ -1,37 +1,30 @@
 import { readFileSync, writeFileSync } from 'fs';
 
 const targetVersion = process.env.npm_package_version;
-const appFolder = './apps/plugin/src/assets';
-const targetFolder = './dist/apps/plugin';
+const pluginMetadataFolder = './';
 const manifestFile = 'manifest.json';
 const versionsFile = 'versions.json';
 
 // read minAppVersion from manifest.json and bump version to target version
 console.log('Generating manifest');
-let manifest = JSON.parse(readFileSync(`${appFolder}/${manifestFile}`, 'utf8'));
+let manifest = JSON.parse(
+  readFileSync(`${pluginMetadataFolder}/${manifestFile}`, 'utf8')
+);
 const { minAppVersion } = manifest;
 manifest.version = targetVersion;
 writeFileSync(
-  `${appFolder}/${manifestFile}`,
-  JSON.stringify(manifest, null, '\t')
-);
-// Replace write by a copy
-writeFileSync(
-  `${targetFolder}/${manifestFile}`,
+  `${pluginMetadataFolder}/${manifestFile}`,
   JSON.stringify(manifest, null, '\t')
 );
 
 // update versions.json with target version and minAppVersion from manifest.json
 console.log('Updating versions file');
-let versions = JSON.parse(readFileSync(`${appFolder}/${versionsFile}`, 'utf8'));
+let versions = JSON.parse(
+  readFileSync(`${pluginMetadataFolder}/${versionsFile}`, 'utf8')
+);
 versions[targetVersion] = minAppVersion;
 
 writeFileSync(
-  `${appFolder}/${versionsFile}`,
-  JSON.stringify(versions, null, '\t')
-);
-// Replace write by a copy
-writeFileSync(
-  `${targetFolder}/${versionsFile}`,
+  `${pluginMetadataFolder}/${versionsFile}`,
   JSON.stringify(versions, null, '\t')
 );
