ea78735ac08b8bf3d55295bde808b484cee56de0
Sebastien Dubois <seb@dsebastien.net>
2024-05-09 15:24:25 +0200

    Removed unused code

--- apps/plugin/src/app/utils/assert-unreachable.fn.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Function that ensures that the associated code is unreachable
- * Reference: https://stackoverflow.com/questions/39419170/how-do-i-check-that-a-switch-block-is-exhaustive-in-typescript
- * @param _x never!
- */
-export function assertUnreachable(_x: never): never {
-  throw new Error("Didn't expect to get here");
-}
