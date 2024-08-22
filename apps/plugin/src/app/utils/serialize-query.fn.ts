230dfb33f108c41b581a5f510de5a1916e0f9a0a
David Buchan <dcb@Dumbledore.localdomain>
2024-08-22 19:53:12 +1000

    Initial proof of concept

--- apps/plugin/src/app/utils/serialize-query.fn.ts
+++ apps/plugin/src/app/utils/serialize-query.fn.ts
@@ -21,10 +21,24 @@ export const serializeQuery = async (
       params.originFile
     );
     // Reference: https://github.com/dsebastien/obsidian-dataview-serializer/issues/3
+
+    // Set up to match the pattern
+    // [[path to note\|alias]] - we are only interested in the path and \| that follow it
+    const linkExp = new RegExp(/\[\[(.+?\\\|).+?\]\]/g);
+
     if (params.query.toLocaleLowerCase().contains('table')) {
       serializedQuery = serializedQuery
         .replaceAll('\\\\', '\\')
         .replaceAll('\n<', '<');
+
+      // Returned links are delivered as the full path to the .md (or other filetype) file, aliased to the note name
+      const matchedLinks = [...serializedQuery.matchAll(linkExp)];
+      for (const match of matchedLinks) {
+        // Matched array
+        // 0: Full matched string
+        // 1: Matched group 1 = filename
+        serializedQuery = serializedQuery.replace(match[1], '');
+      }
     }
   } catch (err: unknown) {
     log('Failed to serialize query', 'warn', err);
