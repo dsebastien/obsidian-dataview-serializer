e4a4a3a0a44d86670ed34637f92f595c956e0404
David Buchan <david@buchan.org>
2024-08-25 18:33:03 +1000

    Revert settings changes

--- apps/plugin/src/app/types/index.ts
+++ apps/plugin/src/app/types/index.ts
@@ -1,11 +1,9 @@
 export interface PluginSettings {
   foldersToScan: string[];
   ignoredFolders: string[];
-  enablePureLinks: boolean;
 }
 
 export const DEFAULT_SETTINGS: PluginSettings = {
   foldersToScan: [],
   ignoredFolders: [],
-  enablePureLinks: false,
 };
